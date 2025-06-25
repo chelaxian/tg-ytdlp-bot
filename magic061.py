b02690ca3f1b14d31c6b5cdc1139835065c05522
ratu.sh <69438111+chelaxian@users.noreply.github.com>
2025-06-25 13:00:56 +0300

    Update and rename magic060.py to magic061.py

--- magic061.py
+++ magic061.py
@@ -1,3 +1,16 @@
+# Version 1.7.9 - Add playlist support to down_and_audio function
+# Version 1.0.0 - Добавлена команда /settings с меню настроек
+# Version 1.0.1 - Settings menu: unique callbacks, English text, correct Back emoji
+# Version 1.0.2 - Settings menu: кнопки вызывают обработчики команд напрямую
+# Version 1.0.4 - Исправлен fake_message: всегда есть chat.first_name и first_name
+# Version 1.0.5 - Исправлен fake_message для /format (command), /audio теперь только подсказка
+# Version 1.0.6 - /save_as_cookie в меню теперь отправляет подсказку из Config
+# Version 1.0.7 - Выровнены надписи на кнопках COOKIES с помощью невидимых символов
+# Version 1.0.8 - Выровнены надписи на кнопках LOGS и MEDIA с помощью невидимых символов
+# Version 1.0.9 - Выровнены надписи на всех кнопках меню /settings с помощью обычных пробелов
+# Version 1.0.10 - Названия /команд на кнопках меню выделены жирным
+# Version 1.0.11 - Исправлено форматирование: жирный убран, пробелы заменены на _
+
 import pyrebase
 import re
 import os
@@ -844,7 +857,7 @@ def remove_media(message, only=None):
         else:
             files = [fname for fname in allfiles if fname.endswith(extension)]
         for file in files:
-            if extension == '.txt' and file in ['logs.txt', 'format.txt', 'tags.txt', 'split.txt']:
+            if extension == '.txt' and file in ['mediainfo.txt', 'logs.txt', 'format.txt', 'tags.txt', 'split.txt']:
                 continue
             file_path = os.path.join(dir, file)
             try:
