e21c307fc09f7110fe90f847b06e00454367d2f7
ratu.sh <69438111+chelaxian@users.noreply.github.com>
2025-06-27 19:35:41 +0300

    Update and rename magic122.py to magic123.py

--- magic123.py
+++ magic123.py
@@ -1,4 +1,4 @@
-#Version 2.1.2
+#Version 2.1.1 
 import pyrebase
 import re
 import os
@@ -3987,8 +3987,8 @@ def ask_quality_menu(app, message, url, tags, playlist_start_index=1):
         # Block with qualities
         if table_block:
             cap += f"\n<blockquote>{table_block}</blockquote>\n"
-        # Hint as a separate quote at the very bottom
-        hint = "<blockquote>📹 — Choose quality for new download.\n🚀 — Instant repost. Video is already saved.</blockquote>"
+        # Hint as a separate code block at the very bottom
+        hint = "<pre>📹 — Choose quality for new download.\n🚀 — Instant repost. Video is already saved.</pre>"
         cap += f"\n{hint}\n"
         buttons = []
         for height in available_heights:
