5fda2bc0ff6829504492ad7b9eb457999290fdc7
ratu.sh <69438111+chelaxian@users.noreply.github.com>
2025-07-03 19:54:00 +0300

    Update and rename magic8.py to magic9.py

--- magic9.py
+++ magic9.py
@@ -2626,7 +2626,7 @@ def down_and_audio(app, message, url, tags, quality_key=None, playlist_name=None
                'outtmpl': os.path.join(user_folder, "%(title)s.%(ext)s"),
                'progress_hooks': [progress_hook],
                'impersonate': 'chrome',
-               'referer': url,
+               #'referer': url,
                'geo_bypass': True,
                'check_certificate': False,
                'live_from_start': True,
@@ -3126,7 +3126,7 @@ def down_and_up(app, message, url, playlist_name, video_count, video_start_with,
                 'playlist_items': str(current_index),  # We use only current_index for playlists
                 'outtmpl': os.path.join(user_dir_name, "%(title)s.%(ext)s"),
                 'impersonate': 'chrome',
-                'referer': url,
+                #'referer': url,
                 'geo_bypass': True,
                 'check_certificate': False,
                 'live_from_start': True
@@ -4498,7 +4498,7 @@ def get_video_formats(url, user_id=None, playlist_start_index=1):
         'simulate': True,
         'playlist_items': str(playlist_start_index),
         'impersonate': 'chrome',
-        'referer': url,
+        #'referer': url,
         'geo_bypass': True,
         'check_certificate': False,
         'live_from_start': True
