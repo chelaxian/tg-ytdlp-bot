b11da92e824e7a0f5de094f1676f360fa9d53b94
ratu.sh <69438111+chelaxian@users.noreply.github.com>
2025-07-14 10:55:37 +0300

    Update and rename magic329.py to magic330.py

--- magic330.py
+++ magic330.py
@@ -3247,10 +3247,10 @@ def down_and_audio(app, message, url, tags, quality_key=None, playlist_name=None
                 # Send full error message with instructions immediately
                 send_to_all(
                     message,
-                    "> Check [here](https://github.com/chelaxian/tg-ytdlp-bot/wiki/yt%E2%80%90dlp-supported-site-list) if your site supported\n"
+                    "> Check [here](https://github.com/chelaxian/tg-ytdlp-bot/wiki/YT_DLP#supported-sites) if your site supported\n"
                     "> You may need `cookie` for downloading this audio. First, clean your workspace via **/clean** command\n"
                     "> For Youtube - get `cookie` via **/download_cookie** command. For any other supported site - send your own cookie ([guide1](https://t.me/c/2303231066/18)) ([guide2](https://t.me/c/2303231066/22)) and after that send your audio link again.\n"
-                    f"\n────────────────\n❌ Error downloading: {error_text}"
+                    f"────────────────\n❌ Error downloading: {error_text}"
                 )
                 return None
             except Exception as e:
@@ -3857,10 +3857,10 @@ def down_and_up(app, message, url, playlist_name, video_count, video_start_with,
                 # Send full error message with instructions immediately
                 send_to_all(
                     message,                   
-                    "> Check [here](https://github.com/chelaxian/tg-ytdlp-bot/wiki/yt%E2%80%90dlp-supported-site-list) if your site supported\n"
+                    "> Check [here](https://github.com/chelaxian/tg-ytdlp-bot/wiki/YT_DLP#supported-sites) if your site supported\n"
                     "> You may need `cookie` for downloading this video. First, clean your workspace via **/clean** command\n"
                     "> For Youtube - get `cookie` via **/download_cookie** command. For any other supported site - send your own cookie ([guide1](https://t.me/c/2303231066/18)) ([guide2](https://t.me/c/2303231066/22)) and after that send your video link again.\n"
-                    f"\n────────────────\n❌ Error downloading: {error_message}"
+                    f"────────────────\n❌ Error downloading: {error_message}"
                 )
                 return None
             except Exception as e:
